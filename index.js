e05563626dfbc8f4b7ed2ecb8770949b70f46859
Vaibhav Pujari <vaibhav276@yahoo.co.in>
2018-06-15 13:50:32 +0530

    Noun replacement, plus UI enhancements

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(function() {
+   $('#icon').hide();
 
    $('#userDialogueText').on('keypress', (e) => {
       if (e.which != 13) {
@@ -7,7 +8,7 @@ $(function() {
       let userDialogue = $('#userDialogueText').val();
       //console.log("User dialogue", userDialogue );
       let answerIndex = 0;
-      let answer = 'Not enough to think about, really';
+      let answer = userDialogue + '? Not enough to think about, really';
       try {
          answerIndex = getApproxDialogue(userDialogue, dialogue) + 1;
          //console.log("Answer index: ", answerIndex);
@@ -19,18 +20,30 @@ $(function() {
             answer.concat(dialogue[answerIndex + 1]);
          }
 
+         answer = replaceNouns(answer, userDialogue);
+
       } catch(err) {
       }
 
-      let answerHtml = '<br/><h3>You: ' + userDialogue + '</h3>';
-      answerHtml += '<h3>Avengers: ' + answer + '</h3>';
-
-      let prevHtml = $('#output').html();
-      $('#output').html(answerHtml + prevHtml);
+      let answerHtml = '<p><b>You: </b><span class="">' + userDialogue + '</span>';
+      appendChatResponse(answerHtml);
+      $('#inputDiv').addClass('loading');
+      $('#icon').show();
+      setTimeout(() => {
+         answerHtml = '<p><b>Avengers: </b><span class="">' + answer + '</span>';
+         appendChatResponse(answerHtml);
+         $('#inputDiv').removeClass('loading');
+         $('#icon').hide();
+      }, 1000 * getRandomInt(3));
 
       $('#userDialogueText').val('');
       return true;
    });
+
+   function appendChatResponse(str) {
+      let prevHtml = $('#output-content').html();
+      $('#output-content').html(prevHtml + str);
+   }
 
    function getRandomInt(max) {
       return Math.floor(Math.random() * Math.floor(max));
@@ -52,11 +65,10 @@ $(function() {
       while (isMatch == false) {
          index = getRandomInt(systemDialogue.length);
          randomSystemDialogue = systemDialogue[index];
-         //console.debug("Evaluating: ", randomSystemDialogue);
+         // console.debug("Evaluating: ", randomSystemDialogue);
          systemDialoguePos = getPos(randomSystemDialogue);
-         let d = dist(userDialoguePos, systemDialoguePos);
-         //console.debug("Distance: ", d);
-         if (d < 3.0) {
+         let score = calcScore(userDialoguePos, systemDialoguePos);
+         if (score < 3.0) {
             continue;
          }
          //console.log("Found system dialog with same number of nouns: ", randomSystemDialogue);
@@ -66,22 +78,12 @@ $(function() {
       return index;
    }
 
-   function countNouns(posArray) {
-      let count = 0;
-      for(let i = 0; i < posArray.length; i++) {
-         if(posArray[i] === 'nn') {
-            count = count + 1;
-         }
-      }
-      return count;
-   }
-
-   function dist(arr1, arr2) {
+   function calcScore(arr1, arr2) {
       let score = 0;
       let arr1Group = _.countBy(arr1);
       let arr2Group = _.countBy(arr2);
-      //console.debug("arr1Group", arr1Group);
-      //console.debug("arr2Group", arr2Group);
+      // console.debug("arr1Group", arr1Group);
+      // console.debug("arr2Group", arr2Group);
       _.mapObject(arr1, (key, val) => {
          //console.debug("checking key ", key);
          if (arr2Group[key] != undefined) {
@@ -91,4 +93,47 @@ $(function() {
       });
       return score;
    }
+
+   function extractNouns(str) {
+      let ris = new RiString(str);
+      let words = ris.words();
+      let pos = ris.pos();
+      let res = [];
+      for (let i = 0; i < words.length; i++) {
+         if (pos[i] === 'nn') {
+            res.push(words[i]);
+         }
+      }
+      return res;
+   }
+
+   function replaceNouns(target, source) {
+      // console.debug('source', source);
+      // console.debug('target', target);
+      let sourceNouns = extractNouns(source);
+      let targetNouns = extractNouns(target);
+      // console.debug("Source Nouns ", sourceNouns);
+      // console.debug("Target Nouns ", targetNouns);
+
+      let ris = new RiString(target);
+      let words = ris.words();
+      let pos = ris.pos();
+      let count = sourceNouns.length;
+      for (let i = 0; i < words.length; i++) {
+         // console.debug("Should I replace : ", words[i]);
+         if (pos[i] === 'nn') {
+            let randomSourceNoun = _.sample(sourceNouns);
+            // console.debug("Replacing %s with %s", words[i], randomSourceNoun);
+            words[i] = randomSourceNoun;
+            count -= 1;
+         }
+         if (count <= 0) {
+            break;
+         }
+      }
+
+      // console.debug('new target', words);
+      return words.join(' ');
+   }
+
 });
